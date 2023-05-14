c6cd48fd88c9f46bf74107e6821e6447d20b0957
LAPTOP-0ST7T81P\Shivam <shivamshokeen14@gmail.com>
2023-05-14 11:09:28 +0530

    Backup app js for book

--- src/App.js
+++ src/App.js
@@ -1,51 +1,14 @@
 import "./App.css";
-import { Fragment, useEffect } from "react";
-import Book from "./Components/Books/Book";
-import BookCard from "./Components/Books/BookCard";
-import useBookContext from "./hooks/use-bookContext";
+import { Fragment } from "react";
 
 function App() {
-  const { apiRequest, bookList } = useBookContext();
-
-  let renderBooks = bookList.map((v, i) => {
-    return (
-      <Fragment key={i}>
-        <BookCard
-          className="mt-4"
-          key={i}
-          textName={v?.photographer}
-          content={v?.photographer}
-          image={v?.src?.original}
-          alt={v?.alt}
-          id={v?.id}
-          name={v?.name}
-          footerImage="/Assets/Images/heart.svg"
-        ></BookCard>
-      </Fragment>
-    );
-  });
-
-  // API get Request on refresh
-
-  useEffect(() => {
-    apiRequest("get", {}, []);
-  }, []);
-
-  // API get Request on refresh
-
   return (
     <Fragment>
       <div className="container-fluid">
-        {/* Input to add books */}
-        <Book></Book>
-        {/* Input to add books */}
-
         <br></br>
 
         <div className="row">
-          <div className="col">
-            <div style={{ display: "inline-flex" }}>{renderBooks}</div>
-          </div>
+          <div className="col">Hello</div>
         </div>
       </div>
       <hr style={{ border: "4px dotted black" }}></hr>
