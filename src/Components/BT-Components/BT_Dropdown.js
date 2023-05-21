41671edc212f380bbd8d4ea749df558fc7a4a0da
LAPTOP-0ST7T81P\Shivam <shivamshokeen14@gmail.com>
2023-05-21 10:58:45 +0530

    code format update

--- src/Components/BT-Components/BT_Dropdown.js
+++ src/Components/BT-Components/BT_Dropdown.js
@@ -1,34 +1,56 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 function BT_Dropdown(props) {
   let renderElement = [];
+  let renderSelectElement = "";
+  const [selectedOption, setSelectedOption] = useState("");
 
   if (props?.dropdownList?.length > 0) {
     renderElement = props.dropdownList.map((v) => {
       let optionRender = v?.option.map((o) => {
         return (
           <Fragment>
-            <option value={o.value}>{o?.name}</option>
+            <option key={o.value + 1} value={o.value}>{o?.name}</option>
           </Fragment>
         );
       });
       return (
         <Fragment>
-          <label className="form-label mb-2">{v?.label}</label>
-          <select className="form-select mb-2">
-            <option selected>{v.placeholder}</option>
+          <label className="form-label mb-2" key={v?.id + 1}>
+            {v?.label}
+          </label>
+          <select
+            key={v?.id + 2}
+            className="form-select mb-2"
+            onClick={(e) => {
+              setSelectedOption(e.target.value);
+            }}
+          >
+            <option selected disabled>{v.placeholder}</option>
             {optionRender}
           </select>
         </Fragment>
       );
     });
+
   } else {
     renderElement = (
       <div className="d-flex justify-content-center">No data found!</div>
     );
   }
 
-  return <Fragment>{renderElement}</Fragment>;
+  if (selectedOption !== "") {
+    renderSelectElement = <div>{selectedOption}</div>;
+  } else {
+    renderSelectElement = <div>Please select a value from Dropdown</div>;
+  }
+
+  return (
+    <Fragment>
+      <div>{renderSelectElement}</div>
+      <div>{renderElement}</div>
+    </Fragment>
+  );
 }
 
 export default BT_Dropdown;
