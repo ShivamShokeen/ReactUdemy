4efbb15b4c45c85273931035d151523f08c99779
LAPTOP-0ST7T81P\Shivam <shivamshokeen14@gmail.com>
2023-05-21 10:41:53 +0530

    Dropdown css

--- src/Components/BT-Components/BT_Dropdown.js
+++ src/Components/BT-Components/BT_Dropdown.js
@@ -1,29 +1,23 @@
 import React, { Fragment } from "react";
 
 function BT_Dropdown(props) {
-  
   let renderElement = [];
 
   if (props?.dropdownList?.length > 0) {
     renderElement = props.dropdownList.map((v) => {
-      
       let optionRender = v?.option.map((o) => {
         return (
           <Fragment>
-            <option value={o.value}>{ o?.name }</option>
+            <option value={o.value}>{o?.name}</option>
           </Fragment>
-        )
-      })
+        );
+      });
       return (
         <Fragment>
           <label className="form-label mb-2">{v?.label}</label>
-          <select class="form-select mb-2">
+          <select className="form-select mb-2">
             <option selected>{v.placeholder}</option>
             {optionRender}
-
-            {/* <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option> */}
           </select>
         </Fragment>
       );
@@ -33,12 +27,8 @@ function BT_Dropdown(props) {
       <div className="d-flex justify-content-center">No data found!</div>
     );
   }
-  
-  return (
-    <Fragment>
-      {renderElement}
-    </Fragment>
-  );
+
+  return <Fragment>{renderElement}</Fragment>;
 }
 
 export default BT_Dropdown;
